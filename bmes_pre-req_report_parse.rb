2bd7ec965376c72b34b70da6b7e8eb72a227a9bb
bycdiaz <cdiaz86@gmail.com>
2019-07-19 21:33:57 -0400

    Adds ability to display pre & post requisites.

--- bmes_pre-req_report_parse.rb
+++ bmes_pre-req_report_parse.rb
@@ -1,3 +1,25 @@
 require 'csv'
+puts "This program will tell you the pre-reqs for the course you enter as well as what the course is a pre-req for."
+puts "What course are you interested in?"
+desired_course = gets.chomp
+puts "You have entered #{desired_course}."
+pre_reqs = []
+post_reqs = []
+CSV.foreach("bmes_pre-req_report.csv") do |row|
+  if desired_course == row[2]
+    if row[20].start_with?('This')
+      pre_reqs << row[20]
+    else
+      pre_reqs << "#{row[20]} is a pre-req for #{row[2]}."
+    end
+    
+  end
 
-puts CSV.read("bmes_pre-req_report.csv")
+  if desired_course == row[20]
+    post_reqs << "#{desired_course} is a pre-req for #{row[2]}."
+  end
+end
+
+puts pre_reqs.sort.uniq
+puts
+puts post_reqs.sort.uniq
