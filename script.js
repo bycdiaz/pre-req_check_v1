f4f8456458b77a5ce4741d608e667ccd4d8086a9
Briggs Elsperger <briggs.elsperger@hotmail.com>
2019-07-22 22:18:06 -0500

    Grabs and parses CSV into array of objects

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-const csvfile = document.getElementById('fileItem').files[0];
+let csvData;
+let csvHeader;
 
-const config = {
-	quotes: false, //or array of booleans
-	quoteChar: '"',
-	escapeChar: '"',
-	delimiter: ",",
-	header: true,
-	newline: "\r\n",
-	skipEmptyLines: false, //or 'greedy',
-	columns: null //or array of strings
+fetch('/pre-req_check/data/all_courses_pre-reqs.csv')
+ .then((response) => {
+    return response.text();
+  })
+  .then((csv) => {
+    parseCSV(csv);
+  });
+
+
+function parseCSV(csv) {
+  const arrayOfRows = csv.split('\n');
+
+  // iterate through array of strings and split on comma
+	const arrayOfCSVData = arrayOfRows.map((row) => {
+		return row.split(',');
+	});
+
+	const header = arrayOfCSVData[0];
+	const body = arrayOfCSVData.slice(1, -1);
+
+	// map header values to body values
+	const jsonData = body.map((arrayOfValues) => {
+		const rowData = {};
+		for(let i = 0; i < arrayOfValues.length; i++) {
+			rowData[header[i]] = arrayOfValues[i];
+		}
+
+		return rowData;
+	});
+
+	csvHeader = header;
+  csvData = jsonData;
+
+	// updateDom()
 }
-
-const data = Papa.parse(csvfile, config)
-
-console.log(data)
