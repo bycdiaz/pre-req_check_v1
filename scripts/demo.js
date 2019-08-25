2386e386a8601c60b400b1f77b30959596425850
bycdiaz <cdiaz86@gmail.com>
2019-08-25 19:08:01 -0400

    Adds event listener to submit button.

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,1 +1,7 @@
 let outputContainer = document.querySelector('#output');
+
+// button event
+let submitButton = document.querySelector('#submit')
+submitButton.addEventListener('click', () => {
+  alert("The Button Works!");
+})
