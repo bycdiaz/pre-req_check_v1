11d7316ff7f7577f56703525718229bacdeedd7c
bycdiaz <cdiaz86@gmail.com>
2019-08-25 20:24:48 -0400

    Adds ability to collect text input.

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,7 +1,10 @@
-let outputContainer = document.querySelector('#output');
-
 // button event
 let submitButton = document.querySelector('#submit')
+
+
+
 submitButton.addEventListener('click', () => {
-  alert("The Button Works!");
+  let enteredCourse = document.querySelector("#courseInput").value;
+  alert(`You've entered ${enteredCourse} in the search box.`);
+  console.log(`You've entered ${enteredCourse} in the search box.`);
 })
