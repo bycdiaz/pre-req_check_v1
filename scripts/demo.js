c9b46c0e63e3f25cdbc75dc45a5653a2d6cc9075
bycdiaz <cdiaz86@gmail.com>
2019-08-25 22:13:37 -0400

    Adds ability to test for 2 courses.

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,10 +1,61 @@
 // button event
-let submitButton = document.querySelector('#submit')
+// function collectCourse() {
+//   let submitButton = document.querySelector('#submit')
+  
+//   submitButton.addEventListener('click', () => {
+//     let enteredCourse = document.querySelector("#courseInput").value;
+//     console.log(`${enteredCourse} was entered in the search box.`);
+//     return enteredCourse;
+//   })
+// };
 
+// collectCourse();
 
+function collectCourse() {
+  let submitButton = document.querySelector('#submit')
+  
+  submitButton.addEventListener('click', () => {
+    let enteredCourse = document.querySelector("#courseInput").value;
+    console.log(`${enteredCourse} was entered in the search box.`);
+    // return enteredCourse;
+    if (enteredCourse === "BMES 338") {
+      // pre-req
+      let prereq = document.querySelector('#prereq');
+      let prereqOutput = document.createElement('p');
+      prereq.appendChild(prereqOutput);
+      prereqOutput.textContent = `${enteredCourse} does not have pre-requisites.`;
 
-submitButton.addEventListener('click', () => {
-  let enteredCourse = document.querySelector("#courseInput").value;
-  alert(`You've entered ${enteredCourse} in the search box.`);
-  console.log(`You've entered ${enteredCourse} in the search box.`);
-})
+      // co-req
+      let coreq = document.querySelector('#coreq');
+      let coreqOutput = document.createElement('p');
+      coreq.appendChild(coreqOutput);
+      coreqOutput.textContent = `${enteredCourse} does not have co-requisites.`;
+
+      // post-req
+      let postreq = document.querySelector('#postreq');
+      let postreqOutput = document.createElement('p');
+      postreq.appendChild(postreqOutput);
+      postreqOutput.textContent = `${enteredCourse} does not have post-requisites.`;
+    } else if (enteredCourse === "BMES 325") {
+      // pre-req
+      let prereq = document.querySelector('#prereq');
+      let prereqOutput = document.createElement('p');
+      prereq.appendChild(prereqOutput);
+      prereqOutput.textContent = `BIO 122 and ENGR 232, or MATH 262 or MATH 210 must be taken before ${enteredCourse}.`;
+
+      // co-req
+      let coreq = document.querySelector('#coreq');
+      let coreqOutput = document.createElement('p');
+      coreq.appendChild(coreqOutput);
+      coreqOutput.textContent = `MEM 202 can be taken concurrently with ${enteredCourse}.`;
+
+      // post-req
+      let postreq = document.querySelector('#postreq');
+      let postreqOutput = document.createElement('p');
+      postreq.appendChild(postreqOutput);
+      postreqOutput.textContent = `${enteredCourse} must be taken before BMES 326.`;
+    }
+  })
+};
+
+collectCourse();
